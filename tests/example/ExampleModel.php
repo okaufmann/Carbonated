83adb292544653a67f3977edc15657ee4850387b
Jesse Leite <jesseleite@gmail.com>
2015-06-21 01:24:07 -0400

    More test setup.

---
+++ tests/example/ExampleModel.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use JerseyMilker\Carbonated;
+
+class ExampleModel extends Illuminate\Database\Eloquent\Model
+{
+    use Carbonated;
+
+    public $carbonatedTimestamps;
+    public $carbonatedDates;
+    public $carbonatedTimes;
+
+    public $carbonatedTimestampFormat;
+    public $carbonatedDateFormat;
+    public $carbonatedTimeFormat;
+    public $carbonatedTimezone;
+
+    public $jsonTimestampFormat;
+    public $jsonDateFormat;
+    public $jsonTimeFormat;
+    public $jsonTimezone;
+
+    public $databaseTimestampFormat;
+    public $databaseDateFormat;
+    public $databaseTimeFormat;
+    public $databaseTimezone;
+}
---
+++ tests/example/Request.php
@@ -0,0 +1,11 @@
+<?php
+
+// !TODO: Figure out how to mock/stub this.
+
+class Request
+{
+    public static function header($contentType)
+    {
+        return 'not jason';
+    }
+}
