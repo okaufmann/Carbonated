1f9df828c892c55e09ef280cac68a49f352d14ac
Oliver Kaufmann <okaufmann91@gmail.com>
2017-04-25 21:44:27 +0200

    Add notice for strftime

--- config/carbonated.php
+++ config/carbonated.php
@@ -3,6 +3,7 @@
 return [
     /*
      * Format dates with carbons formatLocalized method to localize ex. weekday.
+     * Consult http://www.php.net/strftime for placeholders.
      */
     'localization' => false,
 ];
